97c6603cd1d2b32f46530030b838e7513d67cbb2
NikolozLekishvili <K0K0L0z1922@gmail.com>
2026-01-30 21:17:50 +0400

    ,

---
+++ main.js
@@ -0,0 +1,38 @@
+const cells = document.querySelectorAll(".cell");
+const statustext = document.querySelector("#statusText");
+const restartBtn = document.querySelector("#restartBtn");
+const winCodition = [
+    [0,1,2],
+    [3,4,5],
+    [7,8,9],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
+
+let options = ["","","","","","","","",""];
+let currentplayer = "x";
+let running = false;
+
+initializeGame();
+
+function initializeGame(){
+       
+}
+function cellClicked(){
+
+}
+function updateCell(click,index){
+
+}
+function changePlayer(){
+
+}
+function checkWinner(){
+
+}
+function restartGame(){
+
+}
